cff65d5b279871880904066ad433dc435fb909e4
chimaihueze <kihueze@gmail.com>
2022-12-01 22:55:29 +0100

    constructor + getters/setters to ContactManager

--- models/ContactManager.java
+++ models/ContactManager.java
@@ -1,4 +1,22 @@
 package models;
+import java.util.ArrayList;
+
 public class ContactManager {
+    ArrayList<Contact> contacts;
+
+    // constructor
+    public ContactManager() {
+        this.contacts = new ArrayList<Contact>();
+    }
+
+    // getter 
+    public Contact getContacts(int index) {
+        return new Contact(contacts.get(index));
+    }
+
+    // setter
+    public void setContacts(int index, Contact contact) {
+        contacts.set(index, new Contact(contact)); 
+    }
     
 }
