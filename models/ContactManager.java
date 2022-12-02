5eb86e7ea622b0dcf486cbac64409ceb3b7b260c
chimaihueze <kihueze@gmail.com>
2022-12-02 08:59:34 +0100

    enhanced the functionality of ContactManager

--- models/ContactManager.java
+++ models/ContactManager.java
@@ -10,13 +10,40 @@ public class ContactManager {
     }
 
     // getter 
-    public Contact getContacts(int index) {
+    public Contact getContact(int index) {
         return new Contact(contacts.get(index));
     }
 
     // setter
-    public void setContacts(int index, Contact contact) {
+    public void setContact(int index, Contact contact) {
         contacts.set(index, new Contact(contact)); 
+    }
+
+    // addContact
+    public void addContact(Contact contact) {
+        contacts.add(new Contact(contact));
+    }
+
+    // removeContact
+    public void removeContact(String contactName) {
+        if (contacts.isEmpty()) {
+            throw new IllegalStateException("Sorry! cannot remove from am empty list");
+        } else {
+            for (int i = 0; i < contacts.size(); i++) {
+                if (contacts.get(i).getName().equals(contactName)) {
+                    contacts.remove(i);
+                }
+            }
+        }
+    }
+    // toString
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < contacts.size(); i++) {
+            temp += contacts.get(i).toString();
+            temp += "\n\n";
+        } return temp;
+        
     }
     
 }
